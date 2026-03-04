5da3496ebe2a316b63217fde45dacf4348283227
Riza-FP <rizafauzanpratama123@gmail.com>
2026-03-04 10:29:33 +0700

    fix: redis connection error

--- Backend/src/libs/redis.ts
+++ Backend/src/libs/redis.ts
@@ -1,6 +1,6 @@
 import Redis from "ioredis";
 
-const redis = new Redis({
+const redis = process.env.REDIS_URL ? new Redis(process.env.REDIS_URL) : new Redis({
     host: "localhost",
     port: 6379,
 });
